02430670d29beff43b9c9fceb263e19d4cc5af26
Ashna Basnet <ashnabasnet12345@gmail.com>
2025-12-31 14:32:23 +0545

    Add more git commands tutorial

--- scrape.py
+++ scrape.py
@@ -15,6 +15,13 @@
 #git commit git code from github
 
 
+#################################
+# 1 change the code
+#2 git add .
+#3 git commit -m "your message"
+#4 git push
+##################################
+
 import requests  
 from bs4 import BeautifulSoup
 import csv
